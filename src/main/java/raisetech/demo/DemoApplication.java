71af5b8d382a4eb9372308dd0833ff859cae7081
miya2211 <a08018656523@icloud.com>
2023-01-10 19:22:07 +0900

    アドバイスいただいたところを変更いたしました。  - DemoApplication.javaにRestControllerアノテーションを付けて実装せずに、HelloController.javaをつくり、helloメソッドを移動する  - @GetMapping("/datetime")    public String getDateTime() {        LocalDateTime now = LocalDateTime.now();        // now変数を文字列に変換する        return "現在時刻"; // 文字列に変換した値をreturnする    }    を作成する

--- src/main/java/raisetech/demo/DemoApplication.java
+++ src/main/java/raisetech/demo/DemoApplication.java
@@ -1,23 +1,25 @@
 package raisetech.demo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	@GetMapping("/datetime")
+	public String getDateTime() {
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+		return "現在時刻は " + now.format(formatter) + " です。";
 	}
 
 }
-            
